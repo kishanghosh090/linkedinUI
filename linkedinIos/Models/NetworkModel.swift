0ddccd53cd12e5edb3eb313d5bea1360e4858783
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-19 18:35:37 +0530

    add models

---
+++ linkedinIos/Models/NetworkModel.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkModel.swift
+//  linkedinIos
+//
+//  Created by kishan rana ghosh on 19/11/25.
+//
+
+import Foundation
+
+struct NetworkModel {
+    var id : Int
+    var name: String
+    var position: String
+    var mutual: String
+    var image: String
+}
---
+++ linkedinIos/Models/PostModel.swift
@@ -0,0 +1,16 @@
+//
+//  PostModel.swift
+//  linkedinIos
+//
+//  Created by kishan rana ghosh on 19/11/25.
+//
+
+import Foundation
+
+struct PostData {
+    var id: Int
+    var image: String
+    var title: String
+    var followers: Int
+    var profileImage: String
+}
